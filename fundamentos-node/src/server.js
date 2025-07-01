3f432ff2ebe5bd06463a3db5359c1f23a80bda74
Lucas Martini <lucas.leite.martini@gmail.com>
2025-07-01 19:02:12 -0400

    aplicando os estudos sobre streams
    garantindo que os dados sejam lidos por completo para que não haja
    quebra de informações.

---
+++ fundamentos-node/src/server.js
@@ -0,0 +1,38 @@
+import http from 'http';
+
+const users = [];
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  if (method === 'GET' && url === '/users') {
+    return res.setHeader('content-type', 'application/json').end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    });
+    return res.writeHead(201).end('usuario criado');
+  }
+
+  return res.end('Hello World');
+});
+
+server.listen(3333);
