123333eebe52cc090f9f02561b26f95e007a9fcd
Lucas Martini <lucas.leite.martini@gmail.com>
2025-07-03 18:57:32 -0400

    nomeção de grupos de id por regex

--- fundamentos-node/src/utils/build-route-path.js
+++ fundamentos-node/src/utils/build-route-path.js
@@ -1,7 +1,7 @@
 export function buildRoutePath(path) {
   const routParametersRegex = /:([a-zA-Z]+)/g;
 
-  const pathWithParams = path.replaceAll(routParametersRegex, '?<id>([a-z0-9-_]+)');
+  const pathWithParams = path.replaceAll(routParametersRegex, '?<$1>([a-z0-9-_]+)');
 
   const pathRegex = new RegExp(`^${pathWithParams}`);
 
